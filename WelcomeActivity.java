0dc05c53c4f8b5fb1647bb8d0fd0bff054252d00
Ali <72344723+alisabourii@users.noreply.github.com>
2024-03-12 23:49:56 +0300

    Add files via upload

--- WelcomeActivity.java
+++ WelcomeActivity.java
@@ -2,13 +2,21 @@ package com.example.ahmetfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class WelcomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_welcome);
+    }
+    public void register(View view)
+    {
+        startActivity(new Intent(WelcomeActivity.this,RegistrationActivity2.class));
     }
 }
