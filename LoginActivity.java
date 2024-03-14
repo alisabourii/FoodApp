d9c900439c9e5d468c6960785bcbde97e7990d73
Ali <72344723+alisabourii@users.noreply.github.com>
2024-03-14 12:33:12 +0300

    Add files via upload

--- LoginActivity.java
+++ LoginActivity.java
@@ -2,7 +2,9 @@ package com.example.ahmetfoodapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void SingIn(View view){
+
+    }
+    public void register(View view){
+        startActivity(new Intent(LoginActivity.this,RegistrationActivity2.class));
     }
 }
