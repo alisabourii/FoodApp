3f2594a5e546d4d19ba89f17bf55eb417b345455
Ali <72344723+alisabourii@users.noreply.github.com>
2024-03-16 23:14:45 +0300

    Add files via upload

--- LoginActivity.java
+++ LoginActivity.java
@@ -5,6 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,6 +18,24 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
     }
     public void SingIn(View view){
+        EditText edt1 = findViewById(R.id.editTextTextPersonName2);
+        EditText edt2 = findViewById(R.id.editTextTextPersonName3);
+        String mailAdress = edt1.getText().toString();
+        String passWord = edt2.getText().toString();
+
+        ArrayList<String> UsersMailAdress = new ArrayList<String>();
+        UsersMailAdress.add("alisabouri516@gmail.com");
+
+        ArrayList<String> UsersPassWords = new ArrayList<String>();
+        UsersPassWords.add("a1234");
+
+
+        if(UsersMailAdress.contains(mailAdress) && UsersMailAdress.contains(passWord)){
+            if(UsersMailAdress.indexOf(mailAdress) == UsersPassWords.indexOf(passWord)){
+                startActivity(new Intent(LoginActivity.this,FoodPanel.class));
+            }
+        }
+
 
     }
     public void register(View view){
